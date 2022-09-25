0230d8fec789650152e3277babba146049fcb537
Srinivasan-Akash <108281031+Srinivasan-Akash@users.noreply.github.com>
2022-09-25 14:22:43 +0530

    added 1.js

---
+++ assignments/1.js
@@ -0,0 +1,5 @@
+let title = document.querySelectorAll(".crayons-subtitle-2")[1]
+let desc = document.querySelectorAll(".color-base-70")[1]
+
+title.innerText = "Akash Srinivasan"
+desc.innerText = "Greetings to one and all I am Srinivasan Akash a young high school programmer I have expertise in various tech stacks such as web development, android app development, desktop app development, and much more currently I am working as an intern. My friends (engineering students) and mentors asked me to start a youtube channel and clear their doubts hence I had set up a goal to simplify every single tech stack I know. I know various programming languages and frameworks such as HTML, CSS, js, java, python, kotlin, android, type script, angular, react, redux, vue, SQL, MySQL, MongoDB, fireBase, JSON, next.js, brain.js Arduino programming, and digital entrepreneurship and many more"
